3907e1c3038e681ee18717233ddc5a2a86e8853b
Nikita Tsomuk <tsomuk@me.com>
2024-12-04 12:04:56 +0200

    Add basic UI for `MyPlanView`

--- YogaTest/MyPlanView/MyPlanView.swift
+++ YogaTest/MyPlanView/MyPlanView.swift
@@ -12,16 +12,56 @@ struct MyPlanView: View {
     @StateObject var vm = MyPlanViewModel()
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            backgroundImage
+            
+            VStack {
+                Spacer()
+                title
+                Spacer()
+                SessionCardView()
+                    .padding(.horizontal, 40)
+                Spacer()
+                slider
+                Spacer()
+            }
+            
         }
-        .padding()
     }
 }
 
 #Preview {
     MyPlanView()
+}
+
+extension MyPlanView {
+    var backgroundImage: some View {
+        Image(.chapter1Bg)
+            .resizable()
+            .ignoresSafeArea()
+            .scaledToFill()
+    }
+    
+    var title: some View {
+        VStack(spacing: 8) {
+            Text("My Plan".uppercased())
+                .fontWeight(.semibold)
+            Rectangle()
+                .frame(maxWidth: 90, maxHeight: 1)
+            Text("chapter 1".uppercased())
+            
+            Text("Sun Salutation Variation")
+                .fontWeight(.semibold)
+                .frame(maxWidth: 150)
+                .multilineTextAlignment(.center)
+        }
+            
+            .foregroundColor(.black)
+            .padding()
+    }
+    
+    var slider: some View {
+        Rectangle()
+            .frame(maxWidth: .infinity, maxHeight: 40)
+    }
 }
