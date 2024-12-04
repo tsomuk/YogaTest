8fc3d1b95048b90abd06af758f6ffda8e08a3f3b
Nikita Tsomuk <tsomuk@me.com>
2024-12-04 10:22:57 +0200

    Initial Commit

---
+++ YogaTest/YogaTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  YogaTestApp.swift
+//  YogaTest
+//
+//  Created by Nikita Tsomuk on 04.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct YogaTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
